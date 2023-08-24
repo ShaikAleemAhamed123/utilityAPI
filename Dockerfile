9024794f9f560f0a143af06cfa8d9a0a586abf12
Aleem Ahamed <shaikaleemahamed786@gmail.com>
2023-08-25 00:22:48 +0530

    corrected docker file commands

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:18-jdk-alpine
+RUN mvn clean package
 COPY target/utilityAPI-0.0.1-SNAPSHOT.jar utilityAPI-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java","-jar","/utilityAPI-0.0.1-SNAPSHOT.jar"]
 EXPOSE 8080
---
+++ src/test/java/com/utility/utilityAPI/UtilityApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.utility.utilityAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
