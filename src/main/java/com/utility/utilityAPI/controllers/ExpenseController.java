6b524dd040b9c749084bac874b2b6eea2b1b7fed
Aleem Ahamed <shaikaleemahamed786@gmail.com>
2023-08-24 20:55:46 +0530

    Secured the /expense/addExpense end point

--- src/main/java/com/utility/utilityAPI/controllers/ExpenseController.java
+++ src/main/java/com/utility/utilityAPI/controllers/ExpenseController.java
@@ -3,7 +3,9 @@ package com.utility.utilityAPI.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.utility.utilityAPI.models.Expense;
+import com.utility.utilityAPI.services.AuthService;
 import com.utility.utilityAPI.services.ExpenseService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,14 +14,17 @@ import org.springframework.web.bind.annotation.*;
 public class ExpenseController {
 
     ExpenseService expenseService;
-    public ExpenseController(ExpenseService expenseService){
+    AuthService authService;
+    public ExpenseController(ExpenseService expenseService, AuthService authService){
         this.expenseService=expenseService;
+        this.authService=authService;
     }
     @PostMapping("/addExpense")
-    public boolean addExpense(@RequestBody String body){
+    public ResponseEntity<?> addExpense(@RequestBody String body, @RequestHeader("Authorization") String authorizationHeader, @RequestHeader("userHandle") String userHandle){
+        if(!authService.verifyToken(authorizationHeader, userHandle)) return ResponseEntity.status(401).body("User not authenticated");
         Expense expense=ExpenseJsonParser(body);
-        if(expense==null) return false;
-        return expenseService.addExpense(expense);
+        if(expense==null) return ResponseEntity.status(500).body("Difficulty adding the expense to the database !");
+        return ResponseEntity.status(200).body(expenseService.addExpense(expense));
     }
 
     public Expense ExpenseJsonParser(String data){
