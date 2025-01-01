4a71aa3d2b3a2b8ece605fa3ca49f87348f1bab8
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2025-01-01 23:14:44 +0530

    changed database details

--- src/main/java/com/utility/utilityAPI/services/AuthService.java
+++ src/main/java/com/utility/utilityAPI/services/AuthService.java
@@ -12,37 +12,41 @@ public class AuthService {
     UserRepo userRepo;
     PasswordEncoder passwordEncoder;
     JwtGenerator jwtGenerator;
-    public AuthService(UserService userService, UserRepo userRepo, PasswordEncoder passwordEncoder, JwtGenerator jwtGenerator){
-        this.userService=userService;
-        this.userRepo=userRepo;
-        this.passwordEncoder=passwordEncoder;
-        this.jwtGenerator=jwtGenerator;
+
+    public AuthService(UserService userService, UserRepo userRepo, PasswordEncoder passwordEncoder,
+            JwtGenerator jwtGenerator) {
+        this.userService = userService;
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
     }
 
-    public String authenticateUser(String userName, String password){
+    public String authenticateUser(String userName, String password) {
         UserData retrieved_App_userData = userRepo.findByUserHandle(userName);
-        if(retrieved_App_userData ==null) return "";
+        if (retrieved_App_userData == null)
+            return "";
         try {
-            if(passwordEncoder.matches(password, retrieved_App_userData.getPassword())){
+            if (passwordEncoder.matches(password, retrieved_App_userData.getPassword())) {
                 String token = jwtGenerator.generateToken(userName);
-                System.out.println("From the authenticate user method in the user service class :: Token : "+token);
+                System.out.println("From the authenticate user method in the user service class :: Token : " + token);
                 return token;
             }
             return "";
 
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             // Invalid Signing configuration / Couldn't convert Claims.
             return "";
         }
 
     }
+
     public boolean verifyToken(String authorizationHeader, String userName) {
-        String token="";
+        String token = "";
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             token = authorizationHeader.substring(7); // Remove "Bearer " prefix
-            System.out.println("Token extracted successfully ! + token : "+token);
+            System.out.println("Token extracted successfully ! + token : " + token);
             boolean userAvailable = userService.userAvailable(userName);
-            if(!userAvailable){
+            if (!userAvailable) {
                 System.out.println("User not registered !");
                 return false;
             }
